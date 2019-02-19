dc2cd25aa77be7f37e9f7ddda945433ce90a801b
biznet57 <gennadiy@biznet.guru>
2019-02-19 22:47:19 +0200

    Update security.php

--- security.php
+++ security.php
@@ -26,5 +26,22 @@ Options -Indexes
 9.CSRF
 Check user session and priveleges on editing and deleting data in DB
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     if(!isset($_POST['_token']) || $_POST['_token'] !== $_SESSION['_token']){
+     die('Invalid token');
+     }
+}
+
+$_SESSION['_token'] = bin2hex(random_bytes(16)); //or openssl_random_pseudo_bytes(16)//
+
+<form action="" method="post">
+    
+    <input type="hidden" name="_token" value="<?= $_SESSION['_token']; ?>">
+    <input type="submit" value="Delete my account">
+
+</form>
+
+10.User defined file includes
+
 
 */
