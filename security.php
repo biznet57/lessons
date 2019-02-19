820037135685582bb7c3a42fbc74984e10826eeb
biznet57 <gennadiy@biznet.guru>
2019-02-19 22:19:46 +0200

    Create security.php

---
+++ security.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://codecourse.com/watch/php-security?part=96-directory-listing
+
+1.SQL Injection
+
+2.Error reporting
+
+3.Include File extensions
+
+4.XSS
+function e($value){
+return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+}
+
+5.Password hashing
+
+6.Directory listing
+.htaccess
+Options -Indexes
+
+7.HttpOnly Cookies
+
+8. No ids and personal information stored in cookies
+
+9.CSRF
+Check user session and priveleges on editing and deleting data in DB
+
+
+*/
