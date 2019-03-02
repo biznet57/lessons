6d16aec6005ddf65c17daf8b012b1536b932c695
biznet57 <gennadiy@biznet.guru>
2019-03-02 13:07:13 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 parse_url('https://www.biznet.ru/');
 filter_var('dfgd@mail.ru', FILTER_VALIDATE_EMAIL);
 usleep(2000000); // sleep for 2 seconds
+
+function getRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $string = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $string .= $characters[rand(0, strlen($characters) - 1)];
+    }
+
+    return $string;
+}
