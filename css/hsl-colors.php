2ebeb857f43bd68f14023f678fb8203b3b7d2be2
biznet57 <gennadiy@biznet.guru>
2019-03-27 22:43:02 +0200

    Update hsl-colors.php

--- css/hsl-colors.php
+++ css/hsl-colors.php
@@ -14,6 +14,19 @@
   .blue {
     background-color: hsl(240, 50%, 50%);
   }
+  .strip { 
+    border-radius: 20px;
+    width: 70%;
+    height: 400px;
+    margin:  50 auto;
+    background: repeating-linear-gradient(
+      45deg,
+      yellow 0px,
+      yellow 40px,
+      black 40px,
+      black 80px
+    );
+  }
   
   div {
     display: inline-block;
@@ -25,3 +38,5 @@
 <div class="green"></div>
 <div class="cyan"></div>
 <div class="blue"></div>
+
+<div class="strip"></div>
