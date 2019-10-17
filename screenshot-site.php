99030d091f62d2e750a66c7d6fb7bbc4e0742e25
biznet57 <gennadiy@biznet.guru>
2019-10-18 00:06:10 +0300

    Create screenshot-site.php

---
+++ screenshot-site.php
@@ -0,0 +1,4 @@
+https://www.codexworld.com/capture-screenshot-website-url-php-google-api/
+https://base64.guru/developers/php/examples/decode-image
+
+https://stackoverflow.com/questions/757675/website-screenshots
